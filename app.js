21dcc0823040b881cd267d7d93570fb3a2fe3b8f
LucaBlanco <lucablanco0@gmail.com>
2021-12-06 21:56:39 -0300

    agregado carpeta css y archivo styles. Resuelto problema con las imagenes que no cargaban

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+const { Console } = require('console');
 const express = require('express');
 
 const path = require('path');
 
 const app = express();
 
-app.use('static', express.static(__dirname + 'public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//C:\Users\Luca\Desktop\DESAFIO CLASE 4
 
 //llamando archivos .html
 app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, './view/home.html'))});
+
 
 
 //iniciando servidor en puerto:
